21b6cd4c7fbb284f9b4d679cf74738ce2bc002d7
Matthew <mattj20@gmail.com>
2022-08-09 07:04:03 -0700

    commit tree 7e6f68cf685eececb5d0ce45206ac254182c5467

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,17 @@
 module PostsHelper
-  
+    def author_id_field(post)
+        if post.author.nil?
+          select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
+        else
+          hidden_field_tag "post[author_id]", post.author_id
+        end
+    end
+    
+    def author_index(post)
+        if post.author.nil?
+            "No Author Found, maybe try adding one?"
+        else
+            link_to post.author.name, author_posts_path(post.author)
+        end
+    end
 end
